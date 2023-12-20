528734f1619afba796bf988a4aefa8162f485c1c
Peter Phan <100747299+speeDy167@users.noreply.github.com>
2023-12-20 23:44:02 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -51,7 +51,6 @@ pip3 install xlrd
 pip3 install gensim==3.8.3
 pip3 install imbalanced-learn==0.4.0
 pip3 install scikit-learn==1.3.2
-# choose tensorflow-gpu if you've got a fancy GPU
 pip3 install tensorflow==2.8.0
 pip3 install keras==2.8.0
 
@@ -65,14 +64,9 @@ java -jar /path/joern-0.3.1/bin/joern.jar /home/test/source2slice/NVD
 # ===================================== #
 
 # (on screen A: start neo4j service)
-# note: everytime after you finish processing a batch (dir_xxx), 
-#       you should run the following commands and restart the neo4j service;
-#       otherwise the indexing will be problematic
-
 neo4j start-no-wait
 
 # (on screen B: start processing)
-cd $HOME/PSySeVR/source2slice/
 python ./get_cfg_relation.py # this outputs to src/source2slice/cfg_db/
 python ./complete_PDG.py # this outputs to src/source2slice/pdg_db/
 python ./access_db_operate.py # this outputs to src/source2slice/dict_call2cfgNodeID_funcID/
@@ -85,23 +79,16 @@ python ./data_preprocess.py # create in slice_label folder
 # ======================================== #
 # ==== process data: data_preprocess/ ==== #
 # ======================================== #
-
-cd $HOME/PSySeVR/data_preprocess/
-cp $HOME/PSySeVR/source2slice/C/test_data/4 $HOME/PSySeVR/data_preprocess/file/
-
-
-python ./create_hash.py
-python ./delete_list.py
-python ./process_dataflow_func.py
-python ./create_w2vmodel.py
-python ./get_dl_input.py
-python ./dealrawdata.py
-
+python3 ./create_hash.py
+python3 ./delete_list.py
+python3 ./process_dataflow_func.py
+python3 ./create_w2vmodel.py
+python3 ./get_dl_input.py
+python3 ./dealrawdata.py
 # ================================ #
 # ==== model training: model/ ==== #
 # ================================ #
 
-cd $HOME/PSySeVR/model && cp $HOME/PSySeVR/data_preprocess/dataset/test $HOME/PSySeVR/model/dataset && cp $HOME/PSySeVR/data_preprocess/dataset/train $HOME/PSySeVR/model/dataset
 
 python3 ./bgru.py
 
