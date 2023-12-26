6e64d8700a99f44c15fb2dc994dccb518f0eace8
Peter Phan <100747299+speeDy167@users.noreply.github.com>
2023-12-26 16:02:49 +0700

    Reshape update

--- model/bgru.py
+++ model/bgru.py
@@ -3,7 +3,6 @@
 This python file is used to train four class focus data in blstm model
 
 '''
-
 import tensorflow as tf
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.optimizers import SGD, RMSprop, Adagrad, Adam, Adadelta
@@ -11,8 +10,7 @@ from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Masking, Dense, Dropout, Activation, LSTM, GRU, Bidirectional
 from preprocess_dl_Input_version5 import *
 from collections import Counter
-from collections import Counter
-import tensorflow as tf
+
 import numpy as np
 import pickle
 import random
@@ -39,14 +37,15 @@ def build_model(maxlen, vector_dim, layers, dropout):
     model.add(Dropout(dropout))
     
     model.add(Dense(1, activation='sigmoid'))
-          
+  
     # model.compile(loss='binary_crossentropy', optimizer='adamax', metrics=['TP_count', 'FP_count', 'FN_count', 'precision', 'recall', 'fbeta_score'])
     model.compile(loss='binary_crossentropy', optimizer='adamax', metrics=[
         tf.keras.metrics.TruePositives(),
+        tf.keras.metrics.TrueNegatives(),
         tf.keras.metrics.FalsePositives(),
         tf.keras.metrics.FalseNegatives(),
-        tf.keras.metrics.Precision(),
-        tf.keras.metrics.Recall()]
+        'accuracy',
+        'sparse_categorical_accuracy']
     )
     
     model.summary()
@@ -54,20 +53,17 @@ def build_model(maxlen, vector_dim, layers, dropout):
     return model
 
 
-def main(traindataSet_path, testdataSet_path, realtestpath, weightpath, resultpath, batch_size, maxlen, vector_dim, layers, dropout):
+def main(traindataSet_path, testdataSet_path, weightpath, resultpath, batch_size, maxlen, vector_dim, layers, dropout):
     print("Loading data...")
     
     model = build_model(maxlen, vector_dim, layers, dropout)
-    
-    #model.load_weights(weightpath)  #load weights of trained model
     
     print("Train...")
     dataset = []
     labels = []
     testcases = []
     for filename in os.listdir(traindataSet_path):
-        if(filename.endswith(".pkl") is False):
-            continue
+        print(filename)
         f = open(os.path.join(traindataSet_path, filename),"rb")
         dataset_file,labels_file,funcs_file,filenames_file,testcases_file = pickle.load(f)
         f.close()
@@ -78,7 +74,14 @@ def main(traindataSet_path, testdataSet_path, realtestpath, weightpath, resultpa
     bin_labels = []
     for label in labels:
         bin_labels.append(multi_labels_to_two(label))
-    labels = bin_labels
+        
+     
+    labels = np.array(bin_labels) # Convert to NumPy array
+    # Ensure labels are reshaped to match the model output shape (batch_size, 1)
+    #labels = labels.reshape(-1, 1)
+    new_shape = (len(labels), 1)
+    labels = labels.reshape(new_shape)
+    #labels = bin_labels
      
     np.random.seed(RANDOMSEED)
     np.random.shuffle(dataset)
@@ -90,14 +93,14 @@ def main(traindataSet_path, testdataSet_path, realtestpath, weightpath, resultpa
     steps_epoch = int(all_train_samples / batch_size)
     print("start")
     t1 = time.time()
-    model.fit_generator(train_generator, steps_per_epoch=steps_epoch, epochs=10, verbose=0)
+    model.fit(train_generator, steps_per_epoch=steps_epoch, epochs=10)
     t2 = time.time()
     train_time = t2 - t1
 
     model.save_weights(weightpath)
     
     #model.load_weights(weightpath)
-    print("Test1...")
+    print("Test...")
     dataset = []
     labels = []
     testcases = []
@@ -115,87 +118,56 @@ def main(traindataSet_path, testdataSet_path, realtestpath, weightpath, resultpa
         testcases += testcasesfile
         funcs += funcsfiles
         filenames += filenamesfile
-    print(len(dataset), len(labels), len(testcases))
+    #print(len(dataset), len(labels), len(testcases))
 
     bin_labels = []
     for label in labels:
         bin_labels.append(multi_labels_to_two(label))
-    labels = bin_labels
+        
+    labels = np.array(bin_labels) # Convert to NumPy array
+    # Ensure labels are reshaped to match the model output shape (batch_size, 1)
+    #labels = labels.reshape(-1, 1)
+    new_shape = (len(labels), 1)
+    labels = labels.reshape(new_shape)
+    #labels = bin_labels
 
-    batch_size = 1
+    
     test_generator = generator_of_data(dataset, labels, batch_size, maxlen, vector_dim)
     all_test_samples = len(dataset)
     steps_epoch = int(math.ceil(all_test_samples / batch_size))
 
     t1 = time.time()
-    result = model.evaluate_generator(test_generator, steps=steps_epoch)
+    result = model.evaluate(test_generator, steps=steps_epoch)
     t2 = time.time()
     test_time = t2 - t1
-    score, TP, FP, FN, precision, recall, f_score= result[0]
-    f = open("TP_index_blstm.pkl",'wb')
-    pickle.dump(result[1], f)
-    f.close()
-
-    f_TP = open("./result_analyze/BGRU/TP_filenames.txt","ab+")
-    for i in range(len(result[1])):
-        TP_index = result[1][i]
-        f_TP.write(str(filenames[TP_index])+'\n')
-
-    f_FP = open("./result_analyze/BGRU/FP_filenames.txt","ab+")
-    for j in range(len(result[2])):
-        FP_index = result[2][j]
-        f_FP.write(str(filenames[FP_index])+'\n')
-
-    f_FN = open("./result_analyze/BGRU/FN_filenames.txt","a+")
-    for k in range(len(result[3])):
-        FN_index = result[3][k]
-        f_FN.write(str(filenames[FN_index])+'\n')
-
-    TN = all_test_samples - TP - FP - FN
+    print(result)
+    print(result[:4])
+    
+    #TP = result[1]
+    #TN = result[2]
+    #FP = result[3]
+    #FN = result[4]
+    #FPR = float(FP) / (FP + TN + 1e-7)
+    #FNR = float(FN) / (TP + FN + 1e-7)
+    #accuracy = result[5]
+    #precision = float(TP) / (TP + FP + 1e-7)
+    #recall = float(TP) / (TP + FN + 1e-7)
+    #f_score = (2 * precision * recall) / (precision + recall + 1e-7)
+    
+    
     fwrite = open(resultpath, 'a')
     fwrite.write('cdg_ddg: ' + ' ' + str(all_test_samples) + '\n')
     fwrite.write("TP:" + str(TP) + ' FP:' + str(FP) + ' FN:' + str(FN) + ' TN:' + str(TN) +'\n')
-    FPR = float(FP) / (FP + TN)
     fwrite.write('FPR: ' + str(FPR) + '\n')
-    FNR = float(FN) / (TP + FN)
     fwrite.write('FNR: ' + str(FNR) + '\n')
-    Accuracy = float(TP + TN) / (all_test_samples)
-    fwrite.write('Accuracy: ' + str(Accuracy) + '\n')
-    precision = float(TP) / (TP + FP)
+    fwrite.write('Accuracy: ' + str(accuracy) + '\n')
     fwrite.write('precision: ' + str(precision) + '\n')
-    recall = float(TP) / (TP + FN)
     fwrite.write('recall: ' + str(recall) + '\n')
-    f_score = (2 * precision * recall) / (precision + recall)
     fwrite.write('fbeta_score: ' + str(f_score) + '\n')
     fwrite.write('train_time:' + str(train_time) +'  ' + 'test_time:' + str(test_time) + '\n')
     fwrite.write('--------------------\n')
     fwrite.close()
 
-    dict_testcase2func = {}
-    for i in testcases:
-        if not i in dict_testcase2func:
-            dict_testcase2func[i] = {}
-    TP_indexs = result[1]
-    for i in TP_indexs:
-        if funcs[i] == []:
-            continue
-        for func in funcs[i]:
-            if func in dict_testcase2func[testcases[i]].keys():
-                dict_testcase2func[testcases[i]][func].append("TP")
-            else:
-                dict_testcase2func[testcases[i]][func] = ["TP"]
-    FP_indexs = result[1]
-    for i in FP_indexs:
-        if funcs[i] == []:
-            continue
-        for func in funcs[i]:
-            if func in dict_testcase2func[testcases[i]].keys():
-                dict_testcase2func[testcases[i]][func].append("FP")
-            else:
-                dict_testcase2func[testcases[i]][func] = ["FP"]
-    f = open(resultpath+"_dict_testcase2func.pkl",'wb')
-    pickle.dump(dict_testcase2func, f)
-    f.close()
 
 def testrealdata(realtestpath, weightpath, batch_size, maxlen, vector_dim, layers, dropout):
     model = build_model(maxlen, vector_dim, layers, dropout)
@@ -217,15 +189,15 @@ def testrealdata(realtestpath, weightpath, batch_size, maxlen, vector_dim, layer
 
 
 if __name__ == "__main__":
-    batchSize = 32
+    batchSize = 2
     vectorDim = 40
     maxLen = 500
     layers = 2
     dropout = 0.2
-    traindataSetPath = "./dl_input_shuffle/train/"
-    testdataSetPath = "./dl_input_shuffle/test/"
+    traindataSetPath = "./dl_input/train/"
+    testdataSetPath = "./dl_input/test/"
     realtestdataSetPath = "data/"
-    weightPath = './model/BGRU'
-    resultPath = "./result/BGRU/BGRU"
-    main(traindataSetPath, testdataSetPath, realtestdataSetPath, weightPath, resultPath, batchSize, maxLen, vectorDim, layers, dropout)
+    weightPath = './model/BGRU.h5'
+    resultPath = "./result/test.txt"
+    main(traindataSetPath, testdataSetPath, weightPath, resultPath, batchSize, maxLen, vectorDim, layers, dropout)
     #testrealdata(realtestdataSetPath, weightPath, batchSize, maxLen, vectorDim, layers, dropout)
